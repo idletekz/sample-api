3e71e0dd6532b365d4661dcab483bed2b2ea5d1c
r4ng <idletekz@gmail.com>
2022-10-01 16:09:56 -0400

    ui

--- src/frontend-ui/js/script.js
+++ src/frontend-ui/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $.ajax({
-        url: "http://frontend-ui-svc/api"
+        url: "http://frontend-svc:3000/api"
     }).done(function (data) {
         $.each(data, function(k,v){
             $("ul#messages").append("<li class='list-group-item'><strong>" + v["username"] + "</strong>:&nbsp;<i>" + v["message"] + "</i></li>");
