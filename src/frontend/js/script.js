b7659548ae8af02013690fe703e616075d7a0f63
r4ng <idletekz@gmail.com>
2022-10-01 15:56:18 -0400

    fix ingress

--- src/frontend/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $.ajax({
-        url: "http://localhost/api"
-    }).done(function (data) {
-        $.each(data, function(k,v){
-            $("ul#messages").append("<li class='list-group-item'><strong>" + v["username"] + "</strong>:&nbsp;<i>" + v["message"] + "</i></li>");
-        })
-    });
-})
